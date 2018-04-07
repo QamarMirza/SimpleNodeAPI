d8d1eaf6f48f0f6b4d453626fffd90a87af9c8df
QamarMirza <qamar.mirza91@gmail.com>
2018-04-07 00:40:17 -0400

    added get request with id param

--- src/app/routes/note_routes.js
+++ src/app/routes/note_routes.js
@@ -1,6 +1,20 @@
 // routes/note_routes.js
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
-    console.log('routes');
+
+    app.get('/notes/:id', (req, res) => {
+        const id = req.params.id;
+        const details = { '_id': new ObjectID(id) };
+        db.collection('notes').findOne(details, (err, item) => {
+        if (err) {
+            res.send({'error':'An error has occurred'});
+        } else {
+            res.send(item);
+        } 
+        });
+    });
+
     app.post('/notes', (req, res) => {
         const note = { text: req.body.body, title: req.body.title };
         db.collection('notes').insert(note, (err, result) => {
